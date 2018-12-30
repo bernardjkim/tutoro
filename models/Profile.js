6be134f59341a9ee5d40d0daa92cda3f04dffb97
Justin Han <gksksla7140@gmail.com>
2018-12-29 23:14:48 -0800

    add frontend folder

---
+++ models/Profile.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// I dont know what profile should have
+const ProfileSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    image: {
+        type: Number,
+        required: true
+    },
+});
+
+module.exports = Profile = mongoose.model('profiles', ProfileSchema);
