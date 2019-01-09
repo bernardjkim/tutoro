75b5489ae945963746ec2f391f63db71e03a8776
Justin Han <gksksla7140@gmail.com>
2019-01-08 18:20:39 -0800

    language input

--- frontend/src/components/profile/profileForm.js
+++ frontend/src/components/profile/profileForm.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import Select from 'react-select';
+import LanOp from './languages';
+
 class ProfileForm extends React.Component {
     constructor(props) {
         super(props);
@@ -15,13 +18,93 @@ class ProfileForm extends React.Component {
         }
         
     }
+    languageInput = () => {
+        let lan = Object.values(LanOp).map(el=> {
+            return {value: el.name, label: el.nativeName};
+        });
+
+        const {language} = this.state;
+        return (
+            <Select
+                placeholder='Language'
+                value={language}
+                onChange={this.handleChange}
+                options={lan}
+            />
+
+        );
+        
+
+
+
+    }
+
+    majorInput = () => {
+    const options = [{
+            value: '',
+            label: 'select',
+            isDisabled: true
+        },
+        {
+            value: 'Math',
+            label: 'Math'
+        },
+        {
+            value: 'Computer Science',
+            label: 'Computer Science'
+        },
+        {
+            value: 'Meme',
+            label: 'Meme'
+        },
+        {
+            value: 'Art',
+            label: 'Art'
+        },
+        {
+            value: 'Ninja Jutsu',
+            label: 'Ninja Jutsu'
+        },
+    ];
+        const {major} = this.state;
+
+        return (
+            <Select
+                placeholder='Major'
+                value={major}
+                onChange={this.handleChange}
+                options={options}
+            />
+
+        );
+    }
     enrollmentStatusInput =() => {
+        const options = [{
+                value: '',
+                label: 'select',
+                isDisabled: true
+            },
+            {
+                value: 'Enrolled',
+                label: 'Enrolled'
+            },
+            {
+                value: 'Graduated',
+                label: 'Graduated'
+            },
+            {
+                value: 'Dropout',
+                label: 'Dropout'
+            }
+        ];
+        const { enrollmentStatus } = this.state;
         return(
-            <select name="enrollment" required>
-                <option value="" selected disabled>select</option>
-                <option value="Enrolled">Enrolled</option>
-                <option value="Graduated">Graduated</option>
-            </select>
+            <Select
+                placeholder='I am ..'
+                value={enrollmentStatus}
+                onChange={this.handleChange}
+                options={options}
+            />
         );
     }
     
@@ -31,6 +114,8 @@ class ProfileForm extends React.Component {
                 <input type='text' value={this.state.firstName}/>
                 <input type='text' value={this.state.lastName}/>
                 {this.enrollmentStatusInput()}
+                {this.majorInput()}
+                {this.languageInput()}
                 <input type='text' value={this.state.firstName}/>
                 
             </form>
