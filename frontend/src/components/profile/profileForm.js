b4c40c4a7f329d9d9e741d24a90aa14b3750a971
Justin Han <gksksla7140@gmail.com>
2019-01-08 17:04:36 -0800

    start profile form comp

---
+++ frontend/src/components/profile/profileForm.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { connect } from 'react-redux';
+class ProfileForm extends React.Component {
+    constructor(props) {
+        super(props);
+        
+    }
+    
+    render() {
+        return(
+            <div>
+                
+            </div>
+
+        );
+    }
+}
+
+
+const mapStateToProps = state => ({
+});
+
+const mapDispatchToProps = dispatch => ({
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ProfileForm);
