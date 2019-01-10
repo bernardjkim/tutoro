01d67e287f1da271af8cb0b34860f33be26fb00e
Bernard Kim <bernardkim.seattle@gmail.com>
2019-01-09 18:28:34 -0800

    restructure

--- server/server.js
+++ server/server.js
@@ -8,16 +8,12 @@ const dotenv = require("dotenv").config();
 const users = require("./routes/api/user");
 const profile = require("./routes/api/profile");
 
-
 const app = express();
-
-// DB config
-const db = require("./config/keys").mongoURI;
 
 // DB connect
 mongoose
   .connect(
-    db,
+    process.env.mongoURI,
     { useNewUrlParser: true }
   )
   .then(() => console.log("MongoDB connected"))
@@ -33,11 +29,20 @@ app.use(
 
 // Passport Config
 app.use(passport.initialize()); // add passport middleware
-require("./config/passport")(passport);
+require("./utils/passport/passport")(passport);
 
 // routes
 app.use("/api/users", users);
 // app.use('/api/profile', profile);
+
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("frontend/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
--- server/utils/passport/passport.js
+++ server/utils/passport/passport.js
@@ -1,12 +1,11 @@
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
-const mongoose = require('mongoose');
-const User = mongoose.model('users');
-const keys = require('./keys');
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const mongoose = require("mongoose");
+const User = mongoose.model("users");
 
 const options = {};
 options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-options.secretOrKey = keys.secretOrKey;
+options.secretOrKey = process.env.secretOrKey;
 
 module.exports = passport => {
   passport.use(
