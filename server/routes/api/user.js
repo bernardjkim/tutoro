e7c9b0e271e569a144d657b7027a3f9dc289cc09
Bernard Kim <bernardkim.seattle@gmail.com>
2019-01-12 16:29:10 -0800

    upload

--- server/routes/api/user.js
+++ server/routes/api/user.js
@@ -1,22 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcryptjs");
-const jsonwebtoken = require("jsonwebtoken");
 const passport = require("passport");
+const multiparty = require("multiparty");
+const fileType = require("file-type");
+const fs = require("fs");
 
 const User = require("../../models/User");
 const Profile = require("../../models/Profile");
 
 const validateRegisterInput = require("../../utils/validations/signup");
 const validateLoginInput = require("../../utils/validations/login");
+const uploadFile = require("../../utils/S3/uploadFile");
 
-const { encrpyt } = require("../../utils/passwords/passwords");
+const { encrpyt, compare } = require("../../utils/passwords/passwords");
 const { sign } = require("../../jwt/jwt");
 
 // create a user
 router.post("/signup", async (req, res) => {
   const { errors, isValid } = validateRegisterInput(req.body);
-
   if (!isValid) return res.status(400).json(errors);
 
   try {
@@ -32,10 +33,7 @@ router.post("/signup", async (req, res) => {
     });
 
     const savedUser = await user.save();
-    const token = await sign({
-      id: savedUser.id,
-      firstName: savedUser.firstName
-    });
+    const token = await sign({ id: savedUser.id });
 
     return res.status(200).json({ success: true, token });
   } catch (error) {
@@ -44,43 +42,24 @@ router.post("/signup", async (req, res) => {
 });
 
 // login user
-router.post("/login", (req, res) => {
+router.post("/login", async (req, res) => {
   const { errors, isValid } = validateLoginInput(req.body);
+  if (!isValid) return res.status(400).json(errors);
 
-  if (!isValid) {
-    return res.status(400).json(errors);
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) throw Error("This user does not exist");
+
+    const match = await compare(password, user.password);
+    if (!match) throw Error("Incorrect password");
+
+    const token = await sign({ id: user.id });
+    return res.status(200).json({ success: true, token });
+  } catch (error) {
+    return res.status(404).json({ error: error.message });
   }
-
-  const email = req.body.email;
-  const password = req.body.password;
-
-  User.findOne({ email }).then(user => {
-    if (!user) {
-      errors.name = "This user does not exist";
-      return res.status(404).json(errors);
-    }
-
-    bcrypt.compare(password, user.password).then(isMatch => {
-      if (isMatch) {
-        const payload = { id: user.id, firstName: user.firstName };
-
-        jsonwebtoken.sign(
-          payload,
-          process.env.secretOrKey,
-          { expiresIn: 36000 },
-          (err, token) => {
-            res.json({
-              success: true,
-              token: "Bearer " + token
-            });
-          }
-        );
-      } else {
-        errors.password = "Incorrect password";
-        return res.status(400).json(errors);
-      }
-    });
-  });
 });
 
 // validate current user with jwebtoken
@@ -98,49 +77,47 @@ router.get(
 );
 
 // update user profile
-router.post("/:userId/profile", (req, res) => {
-  var userId = req.params.userId;
-  User.findOne({ id: userId }).then(user => {
-    if (!user) {
-      errors.name = "This user does not exist";
-      return res.status(404).json(errors);
+router.post("/:userId/profile", async (req, res) => {
+  const { userId } = req.params;
+
+  if (await !User.findOne({ id: userId }))
+    return res.status(400).json({ error: "This user does not exist" });
+
+  const form = new multiparty.Form();
+  form.parse(req, async (error, fields, files) => {
+    if (error) throw Error(error);
+
+    try {
+      const path = files.file[0].path;
+      const buffer = fs.readFileSync(path);
+      const type = fileType(buffer);
+      const fileName = files.file[0].originalFilename;
+      const data = await uploadFile(buffer, fileName, type);
+
+      const newProfile = new Profile({
+        userId: userId,
+        image: data.key,
+        phone: fields.phone,
+        firstName: fields.firstName,
+        lastName: fields.lastName,
+        enrollment: fields.enrollment,
+        major: fields.major,
+        coursesTaken: fields.coursesTaken,
+        locationPreferences: fields.locationPreferences,
+        languagePreferences: fields.languagePreferences
+      });
+
+      newProfile
+        .save()
+        .then(profile => {
+          return res.status(200).json({ success: true, profile });
+        })
+        .catch(error => {
+          return res.status(400).json({ error: error.message });
+        });
+    } catch (error) {
+      return res.status(400).json({ error: error.message });
     }
-    const form = new multiparty.Form();
-
-    form.parse(request, async (error, fields, files) => {
-      console.log(fields);
-      console.log(files);
-      if (error) throw new Error(error);
-
-      try {
-        const path = files.file[0].path;
-        const buffer = fs.readFileSync(path);
-        const type = fileType(buffer);
-        const fileName = files.file[0].originalFilename;
-        const data = await uploadFile(buffer, fileName, type);
-
-        const newProfile = new Profile({
-          userId: userId,
-          image: data.key,
-          phone: req.body.phone,
-          firstName: req.body.firstName,
-          lastName: req.body.lastName,
-          enrollment: req.body.enrollment,
-          major: req.body.major,
-          coursesTaken: req.body.coursesTaken,
-          locationPreferences: req.body.locationPreferences,
-          languagePreferences: req.body.languagePreferences
-        });
-        newProfile.save().then(profile => {
-          res.json({
-            sucess: true,
-            profile
-          });
-        });
-      } catch (error) {
-        return res.status(400).send(error);
-      }
-    });
   });
 });
 
