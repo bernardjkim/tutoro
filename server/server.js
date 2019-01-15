bfdc54e438f808e193db3f62d27157986ff52080
Bernard Kim <bernardkim.seattle@gmail.com>
2019-01-14 16:21:31 -0800

    merge

--- server/server.js
+++ server/server.js
@@ -45,28 +45,6 @@ require("./utils/passport/passport")(passport);
 app.use("/api/user", user);
 // app.use('/api/profile', profile);
 
-<<<<<<< HEAD
-// Define POST route
-// app.post("/test-upload", (request, response) => {
-//   const form = new multiparty.Form();
-//   form.parse(request, async (error, fields, files) => {
-//     if (error) throw new Error(error);
-//     try {
-//       const path = files.file[0].path;
-//       const buffer = fs.readFileSync(path);
-//       const type = fileType(buffer);
-//       const timestamp = Date.now().toString();
-//       const fileName = `bucketFolder/${timestamp}-lg`;
-//       const data = await uploadFile(buffer, fileName, type);
-//       return response.status(200).send(data);
-//     } catch (error) {
-//       return response.status(400).send(error);
-//     }
-//   });
-// });
-
-=======
->>>>>>> 50e50efd7404c5ebd1098b3d245e11ccb6d1af7d
 // Server static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
